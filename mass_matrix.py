be55e6defd42f34a5c20ed4b18447901833f41a0
jvap2 <jayvap6181@gmail.com>
2022-09-02 08:43:12 -0600

    upper diag added to mass matrix

--- mass_matrix.py
+++ mass_matrix.py
@@ -15,7 +15,8 @@ class MassMatrix(Mesh):
         middle_diag[1:self.N-1]=(1/(2*self.silengths()[0:self.N-2]))*(self.mesh_points()[1:self.N-1]**2-2*self.mesh_points()[0:self.N-2]*(self.mesh_points()[1:self.N-1]-self.midpoints()[0:self.N-2])-self.midpoints()[0:self.N-2]**2)
         middle_diag[1:self.N-1]+=(1/(2*self.silengths()[1:self.N-1]))*(self.mesh_points()[1:self.N-1]**2+2*self.mesh_points()[2:self.N]*(self.midpoints()[1:self.N-1]-self.mesh_points()[1:self.N-1])-self.midpoints()[1:self.N-1]**2)
         lower_diag[0:]=(1/(2*self.silengths()[1:]))*(self.mesh_points()[1:self.N]**2-2*self.mesh_points()[1:self.N]*self.midpoints()[1:]+self.midpoints()[1:]**2)
-        return middle_diag, lower_diag
+        upper_diag[0:]=(1/(2*self.silengths()[:self.N-1]))*(self.midpoints()[:self.N-1]**2-2*self.mesh_points()[:self.N-1]*self.midpoints()[:self.N-1]+self.mesh_points()[:self.N-1]**2)
+        return middle_diag, lower_diag,upper_diag
 
 m=MassMatrix(0,4,5)
 print(m.Construct())
