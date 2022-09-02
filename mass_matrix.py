3b503e9432972ad2781fce283715f1e5d0ccf6e9
jvap2 <jayvap6181@gmail.com>
2022-09-02 14:19:03 -0600

    Constructed Mass matrix, may still be off

--- mass_matrix.py
+++ mass_matrix.py
@@ -12,11 +12,14 @@ class MassMatrix(Mesh):
         middle_diag=np.zeros(self.N)
         upper_diag=np.zeros(self.N-1)
         lower_diag=np.zeros(self.N-1)
+        middle_diag[0]=(1/(2*self.silengths()[0]))*(self.midpoints()[0]**2-2*self.mesh_points()[0]*self.midpoints()[0]+self.mesh_points()[0])
+        middle_diag[self.N-1]=(1/(2*self.silengths()[-1]))*(self.midpoints()[-1]**2-2*self.mesh_points()[-2]*(self.midpoints()[-1]-self.mesh_points()[-1])-self.mesh_points()[-1]**2)
         middle_diag[1:self.N-1]=(1/(2*self.silengths()[0:self.N-2]))*(self.mesh_points()[1:self.N-1]**2-2*self.mesh_points()[0:self.N-2]*(self.mesh_points()[1:self.N-1]-self.midpoints()[0:self.N-2])-self.midpoints()[0:self.N-2]**2)
         middle_diag[1:self.N-1]+=(1/(2*self.silengths()[1:self.N-1]))*(self.mesh_points()[1:self.N-1]**2+2*self.mesh_points()[2:self.N]*(self.midpoints()[1:self.N-1]-self.mesh_points()[1:self.N-1])-self.midpoints()[1:self.N-1]**2)
         lower_diag[0:]=(1/(2*self.silengths()[1:]))*(self.mesh_points()[1:self.N]**2-2*self.mesh_points()[1:self.N]*self.midpoints()[1:]+self.midpoints()[1:]**2)
         upper_diag[0:]=(1/(2*self.silengths()[:self.N-1]))*(self.midpoints()[:self.N-1]**2-2*self.mesh_points()[:self.N-1]*self.midpoints()[:self.N-1]+self.mesh_points()[:self.N-1]**2)
-        return middle_diag, lower_diag,upper_diag
+        M=np.diag(middle_diag,k=0)+np.diag(upper_diag, k=1)+np.diag(lower_diag,k=-1)
+        return M
 
 m=MassMatrix(0,4,5)
 print(m.Construct())
