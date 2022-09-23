f2a7eb539a2f23eb955f783c454ff72c49dfc962
jvap2 <jayvap6181@gmail.com>
2022-09-23 16:01:56 -0600

    First draft of parallelized Mass Matrix Construction

--- Finite_Volume/Quasi-Parallel/d_mass_matrix.py
+++ Finite_Volume/Quasi-Parallel/d_mass_matrix.py
@@ -2,13 +2,9 @@ import cupy as cp
 from d_mesh import Mesh
 
 class MassMatrix():
-    '''
-    Class for construction of Mass Matrix
-    Inherites the attributes of the Mesh class
-    '''
     def __init__(self,a,b,N,t_0,t_m,M):
         self.mesh=Mesh(a,b,N,t_0,t_m,M)
-        self.N=self.mesh.NumofSubIntervals()
+        self.N=N
     def Construct(self):
         x=self.mesh.mesh_points()
         mid=self.mesh.midpoints()
