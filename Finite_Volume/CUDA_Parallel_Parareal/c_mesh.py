41f61a72602012df75e4f35e293cd096b0e94930
jvap2 <jayvap6181@gmail.com>
2022-09-23 12:44:04 -0600

    Still needs tinkering, but working for the most part

--- Finite_Volume/CUDA_Parallel_Parareal/c_mesh.py
+++ Finite_Volume/CUDA_Parallel_Parareal/c_mesh.py
@@ -1,3 +1,4 @@
+from array import array
 from concurrent.futures import thread
 from math import floor
 import numpy as np
@@ -17,16 +18,39 @@ class Mesh():
     def NumofSubIntervals(self):
         return self.N
     def mesh_points(self):
-        array=np.zeros(self.N+2)
         b,a,Nsize=self.b,self.a,self.N+2
-        helper_linspace(array,a,b,Nsize)
-        print(array)
+        array=np.zeros(Nsize)
+        threads_per_block, blocks_per_grid= init_threads_blocks(128, Nsize)
+        d_array=cuda.to_device(array)
+        p_linspace[blocks_per_grid,threads_per_block](d_array, a, b, Nsize)
+        d_array.copy_to_host(array)
+        return array
+    def midpoints(self):
+        Nsize=self.N+1
+        array=np.zeros(Nsize)
+        mesh=self.mesh_points()
+        thread, block = init_threads_blocks(128, Nsize)
+        d_array=cuda.to_device(array)
+        p_midpoints[block,thread](mesh,array,Nsize)
+        d_array.copy_to_host(array)
+        return array
+    def silengths(self):
+        Nsize=self.N+1
+        array=np.zeros(Nsize)
+        mesh=self.mesh_points()
+        helper_interval_lengths(mesh,array,Nsize)
         return array
     def time_points(self):
         array=np.zeros(self.M+1)
         t_0,t_m,M=self.t_0,self.t_m,self.M+1
         helper_linspace(array,t_0,t_m,M)
-        print(array)
+        return array
+    def delta_t(self):
+        Nsize=self.M
+        array=np.zeros(Nsize)
+        time=self.time_points()
+        d_time=cuda.to_device(time)
+        helper_interval_lengths(d_time,array,Nsize)
         return array
 
         
@@ -38,14 +62,41 @@ def p_linspace(array, a, b, Nsize):
     step=(b-a)/(Nsize-1)
     if(idx<Nsize):
         array[idx]=a+(idx*step)
-def helper_linspace(array, a, b, Nsize):
-    t=128
-    b=(Nsize+t-1)//t
-    d_array=cuda.to_device(array)
-    p_linspace[b,t](d_array, a, b, Nsize)
-    d_array.copy_to_host(array)
-    
 
-m=Mesh(-4,4,8,0,1,3)
-m.mesh_points()
-m.time_points()
+
+@cuda.jit('void(float64[:], float64[:], float64)')
+def p_midpoints(mesh, mid, Nsize):
+    idx=cuda.threadIdx.x+(cuda.blockDim.x*cuda.blockIdx.x)
+    if(idx<Nsize):
+        mid[idx]=(mesh[idx]+mesh[idx+1])/2.0
+
+@cuda.jit('void(float64[:], float64[:], float64)')
+def p_interval_lengths(mesh, length, Nsize):
+    idx=cuda.threadIdx.x+(cuda.blockDim.x*cuda.blockIdx.x)
+    if(idx<Nsize):
+        length[idx]=(mesh[idx]-mesh[idx+1])
+
+def helper_linspace(array, a, b, Nsize):
+    threads_per_block, blocks_per_grid= init_threads_blocks(32, Nsize)
+    d_array=cuda.to_device(array)
+    p_linspace[blocks_per_grid,threads_per_block](d_array, a, b, Nsize)
+    d_array.copy_to_host(array)
+
+def helper_interval_lengths(array, return_array, Nsize):
+    threads_per_block, blocks_per_grid= init_threads_blocks(32, Nsize)
+    d_r_array=cuda.to_device(return_array)
+    d_array=cuda.to_device(array)
+    p_interval_lengths[blocks_per_grid,threads_per_block](d_array,return_array,Nsize)
+    d_r_array.copy_to_host(return_array)
+
+def init_threads_blocks(threads, Nsize):
+    threads_per_block=threads
+    blocks_per_grid=(Nsize+threads_per_block-1)//threads_per_block
+    return threads_per_block, blocks_per_grid
+
+m=Mesh(-4,4,1000,0,1,300)
+print(m.mesh_points(),
+m.time_points(),
+m.midpoints(),
+m.silengths(),
+m.delta_t())
