aba0126d322e4e71264238fccdde943719a9692a
jvap2 <jayvap6181@gmail.com>
2023-01-27 04:06:51 -0700

    Alterations- will test

--- Finite_Volume/Quasi-Parallel/d_cgs.py
+++ Finite_Volume/Quasi-Parallel/d_cgs.py
@@ -57,7 +57,7 @@ def B_3_Cubic_Right_Min(N,beta):
 
 
 
-def B_3_Cubic_Right(N,beta):
+def B_3_Cubic_Right_Plus(N,beta):
     j=cp.linspace(1,N-3, N-3)
     B_R_Plus=cp.zeros(shape=(N,3))
     B_R_Plus[0:N-3,2]=(-(N-j+.5)**beta)*(6*(beta**2)+(13-10*N)*beta+10*beta*j+6*(j**2)+j*(14-12*N)+6*(N**2)-14*N+3.5)+\
@@ -81,38 +81,31 @@ def B_3_Cubic_Right(N,beta):
     B_R_Plus[N-5,0]=((.5)**beta)*((-4*beta**2)/3-4*beta-(2/3))-(1.5**beta)*(-2*beta**2-6*beta+23)+(2.5**beta)*((-4*beta**2)/3-4*beta+142/3)-(4.5**beta)*((2*beta**2)/3-7*beta+143/6)
     return B_R_Plus
     
-def B_2_Cubic_Right(N,gamma,beta,h,x=0):
-    k_left_non_lin=lambda x: 1+x
-    K_m_ss=cp.ones(N+1)
-    K_m_ss_nonlin=k_left_non_lin(x)
+def B_2_Cubic_Right_Plus(N,beta):
     row_plus=cp.zeros(shape=N-5)
-    row_min=cp.zeros(shape=N-5)
-    col_min=cp.zeros(shape=N)
     col_plus=cp.zeros(shape=N)
     q=cp.linspace(-2,-(N-4), N-5)
     row_plus[1:]=((-q[1:]+.5)**beta)*((-4*beta**2)/3-4*beta+8*q[1:]**2-8*q[1:]-(2/3))+((-q[1:]-1.5)**beta)*((-4*beta**2)/3-4*beta+8*q[1:]**2+24*q[1:]+(46/3))-\
     (((-q[1:]+1.5)**beta)*((-beta**2)/3-beta+2*q[1:]**2-6*q[1:]+(23/6))+((-q[1:]-.5)**beta)*(-2*beta**2-6*beta+12*q[1:]**2+12*q[1:]-1)+((-q[1:]-2.5)**beta)*((-beta**2)/3-beta+2*q[1:]**2+10*q[1:]+(71/6)))
-    row_min=((-q+2.5)**beta)*((-beta**2)/3-beta+2*q**2-10*q+(71/6))+((-q+.5)**beta)*(-2*beta**2-6*beta+12*q**2-12*q-1)+((-q-1.5)**beta)*((-beta**2)/3-beta+2*q**2+6*q+(23/6))-\
-    (((-q+1.5)**beta)*((-4*beta**2)/3-4*beta+8*q**2-24*q+(46/3))+((-q-.5)**beta)*((-4*beta**2)/3-4*beta+8*q**2+8*q-(2/3)))
     row_plus[0]=(2.5**beta)*((-4*beta**2)/3-4*beta+(142/3))+(.5**beta)*((-4*beta**2)/3-4*beta-(2/3))-((3.5**beta)*((-beta**2)/3-beta+(143/6))+(1.5**beta)*(-2*beta**2-6*beta+23))
     col_plus[0]=row_plus[0]
-    col_min[0]=row_min[0]
     col_plus[1]=(1.5**beta)*((-4*beta**2)/3-4*beta+(46/3))-((2.5**beta)*((-beta**2)/3-beta+71/6)+(.5**beta)*(-2*beta**2-6*beta-1))
-    col_min[1]=-col_plus[0]
     col_plus[2]=(.5**beta)*((-4*beta**2)/3-4*beta-2/3)-(1.5**beta)*((-beta**2)/3-beta+23/6)
-    col_min[2]=-col_plus[1]
     col_plus[3]=(-1)*((.5)**beta)*((-beta**2)/3-beta-1/6)
-    col_min[3]=-col_plus[2]
-    col_plus[4]=(-1)*((.5)**beta)*((-beta**2)/3-beta-1/6)
-    col_min[4]=-col_plus[3]
     B_R_Plus=toeplitz(c=col_plus,r=row_plus)
+    return B_R_Plus
+
+
+def B_2_Cubic_Right_Min(N,beta):
+    row_min=cp.zeros(shape=N-5)
+    col_min=cp.zeros(shape=N)
+    q=cp.linspace(-2,-(N-4), N-5)
+    row_min=((-q+2.5)**beta)*((-beta**2)/3-beta+2*q**2-10*q+(71/6))+((-q+.5)**beta)*(-2*beta**2-6*beta+12*q**2-12*q-1)+((-q-1.5)**beta)*((-beta**2)/3-beta+2*q**2+6*q+(23/6))-\
+    (((-q+1.5)**beta)*((-4*beta**2)/3-4*beta+8*q**2-24*q+(46/3))+((-q-.5)**beta)*((-4*beta**2)/3-4*beta+8*q**2+8*q-(2/3)))
+    col_min[0]=row_min[0]
+    col_min[1]=-(2.5**beta)*((-4*beta**2)/3-4*beta+(142/3))-(.5**beta)*((-4*beta**2)/3-4*beta-(2/3))+((3.5**beta)*((-beta**2)/3-beta+(143/6))+(1.5**beta)*(-2*beta**2-6*beta+23))
+    col_min[2]=-(1.5**beta)*((-4*beta**2)/3-4*beta+(46/3))+((2.5**beta)*((-beta**2)/3-beta+71/6)+(.5**beta)*(-2*beta**2-6*beta-1))
+    col_min[3]=-(.5**beta)*((-4*beta**2)/3-4*beta-2/3)+(1.5**beta)*((-beta**2)/3-beta+23/6)
+    col_min[4]=((.5)**beta)*((-beta**2)/3-beta-1/6)
     B_R_Min=toeplitz(c=col_min,r=row_min)
-    constant=((1-gamma)*(h**(beta-1)))/(2*math.gamma(beta+3))
-    K_plus_diag=constant*diag(K_m_ss_nonlin[1:],k=0)
-    K_min_diag=constant*diag(K_m_ss_nonlin[:N],k=0)
-    B=cp.matmul(K_plus_diag,B_R_Plus)+cp.matmul(K_min_diag,B_R_Min)
-    return B
-
-
-
-
+    return B_R_Min
